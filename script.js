c04406358f3cc29657e932b5698d441438b69e5e
SaishJ <saishjagtap0003@gmail.com>
2022-02-07 19:40:09 +0530

    Variables

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-console.log("Hello");
-console.log("Test");
+var variableName = "Welcome to Variables.";
+console.log(variableName);
+
+let name = "John";
+name = "Saish";
+console.log(name);
+
+const box = "Box 1";
+// box = "Box 2";   got error
+// In const we cannot reassign variables.
+console.log(box);
