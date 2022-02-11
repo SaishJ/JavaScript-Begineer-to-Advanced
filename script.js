936ccba45a65feb9ea578b02d38c3c75123affc1
Saish Jagtap <59008917+SaishJ@users.noreply.github.com>
2022-02-11 20:37:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ const box = "Box 1";
 // box = "Box 2";   got error
 // In const we cannot reassign variables.
 console.log(box);
+
+// block -> A block is a set of opening and closing curly brackets.
+
+// var -> can be reassigned and are only available inside the function they're created in. Its function scoped.
+
+// const -> cannot be reassigned and not accessible before they appear within the code. Its block scoped.
+
+// let -> It can be reassigned but its similar to const i.e block scoped.
+//        If variables are not crreated inside a function or block they are globally scoped.
